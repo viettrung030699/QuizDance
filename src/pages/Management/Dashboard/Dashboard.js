272e6276d9fab5ed9f80aae20fdfe0a5d14291b4
Barry Bear <wokhug99@gmail.com>
2020-12-20 02:48:45 +0700

    Completed dashboard

--- src/pages/Management/Dashboard/Dashboard.js
+++ src/pages/Management/Dashboard/Dashboard.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react'
 import { Bar } from '@ant-design/charts'
-import AntBreadcrumb from '../../../components/Management/Breadcrumb/AntBreadcrumb';
-import { Button, Card } from 'antd';
+import { Button, Card, Table, Space, Carousel, Row, Col } from 'antd';
+import Column from 'antd/lib/table/Column';
+import { CheckCircleTwoTone, CloseCircleTwoTone, WarningTwoTone } from '@ant-design/icons';
 
-const data = [
-  { week: '5', key: 'OOAD', value: 29 },
-  { week: '5', key: 'SPM', value: 30 },
-  { week: '6', key: 'OOAD', value: 15 },
-  { week: '6', key: 'SPM', value: 10 },
-  { week: '7', key: 'OOAD', value: 20 },
-  { week: '7', key: 'SPM', value: 15 },
-  { week: '8', key: 'OOAD', value: 17 },
-  { week: '8', key: 'SPM', value: 21 },
-  { week: '9', key: 'OOAD', value: 27 },
-  { week: '9', key: 'SPM', value: 23 },
-  { week: '10', key: 'OOAD', value: 30 },
-  { week: '10', key: 'SPM', value: 25 },
-  { week: '11', key: 'OOAD', value: 21 },
-  { week: '11', key: 'SPM', value: 29 }
+import AntBreadcrumb from '../../../components/Management/Breadcrumb/AntBreadcrumb';
+import { Fragment } from 'react';
+
+const labChartData = [
+  { week: 'Week 5', key: 'SPM', value: 30 },
+  { week: 'Week 6', key: 'SPM', value: 10 },
+  { week: 'Week 7', key: 'SPM', value: 15 },
+  { week: 'Week 8', key: 'SPM', value: 21 },
+  { week: 'Week 9', key: 'SPM', value: 23 },
+  { week: 'Week 10', key: 'SPM', value: 25 },
+  { week: 'Week 11', key: 'SPM', value: 29 }
 ]
 
-const config = {
-  data: data,
-  width: 1400,
+const labChartConfig = {
+  data: labChartData,
+  width: 1000,
   height: 300,
   autoFit: false,
   isGroup: true,
@@ -36,28 +33,119 @@ const config = {
       fill: '#fff',
     },
   },
-};
+}
+
+const theoryChartData = [
+  { week: 'Week 1', key: 'OOAD', value: 60 },
+  { week: 'Week 1', key: 'Entre', value: 60 },
+  { week: 'Week 2', key: 'OOAD', value: 30 },
+  { week: 'Week 2', key: 'Entre', value: 20 },
+  { week: 'Week 3', key: 'OOAD', value: 15 },
+  { week: 'Week 3', key: 'Entre', value: 20 },
+  { week: 'Week 4', key: 'OOAD', value: 55 },
+  { week: 'Week 4', key: 'Entre', value: 45 },
+  { week: 'Week 5', key: 'OOAD', value: 43 },
+  { week: 'Week 5', key: 'Entre', value: 43 },
+  { week: 'Week 6', key: 'OOAD', value: 12 },
+  { week: 'Week 6', key: 'Entre', value: 20 },
+  { week: 'Week 7', key: 'OOAD', value: 30 },
+  { week: 'Week 7', key: 'Entre', value: 37 },
+  { week: 'Week 8', key: 'OOAD', value: 57 },
+  { week: 'Week 8', key: 'Entre', value: 59 },
+  { week: 'Week 9', key: 'OOAD', value: 10 },
+  { week: 'Week 9', key: 'Entre', value: 23 },
+  { week: 'Week 10', key: 'OOAD', value: 30 },
+  { week: 'Week 10', key: 'Entre', value: 25 },
+  { week: 'Week 11', key: 'OOAD', value: 21 },
+  { week: 'Week 11', key: 'Entre', value: 29 }
+]
+
+const theoryChartConfig = {
+  data: theoryChartData,
+  width: 1000,
+  height: 300,
+  autoFit: false,
+  isGroup: true,
+  xField: 'value',
+  yField: 'week',
+  seriesField: 'key',
+  marginRatio: 0,
+  label: {
+    position: 'left',
+    style: {
+      fill: '#fff',
+    },
+  },
+}
+
+const tableData = [
+  { id: 1, name: 'Software Project Management', room: 'A1.606', type: 'Laboratory', weekday: 'Monday', time: '13:15', useQuiz: true },
+  { id: 2, name: 'Entrepreneurship', room: 'L.202', type: 'Theory', weekday: 'Wednesday', time: '10:30', useQuiz: false },
+  { id: 3, name: 'Object-Oriented Analysis & Design', room: 'A1.409', type: 'Theory', weekday: 'Wednesday', time: '13:15', useQuiz: false }
+]
 
 export default class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        <AntBreadcrumb
-          elements={[
-            { text: 'Admin', to: '/admin' },
-            { text: 'Dashboard' },
-          ]}
-        />
-        <div style={{display:'flex', flexDirection:'row'}}>
-          <Bar {...config} />
-          <Card style={{width:'100%', textAlign:'center'}} title='Upcoming Class'>
-            <h4>A1.606</h4>
-            <h5>Software Project Management Laboratory</h5>
 
-            <Button type='primary' onClick={()=>{}}>Open Session</Button>
-          </Card>
-        </div>
-      </div>
+  render() {
+    const table = (
+      <Table dataSource={tableData} pagination={false} rowKey='id'>
+        <Column title='Room' dataIndex='room' key='room' />
+        <Column title='Course Name' dataIndex='name' key='name' />
+        <Column title='Type' dataIndex='type' key='type' />
+        <Column title='Weekday' dataIndex='weekday' key='weekday' />
+        <Column title='Time' dataIndex='time' key='time' />
+        <Column title='Available Quiz' dataIndex='useQuiz' key='useQuiz'
+          render={useQuiz => useQuiz ?
+            <CheckCircleTwoTone twoToneColor="#52c41a" />
+            : <CloseCircleTwoTone twoToneColor="#FF0000" />}
+        />
+        <Column title="Action" key="action"
+          render={(text, record) => (
+            <Space size="middle">
+              <Button type='link' onClick={() => { }}>Edit Session</Button>
+              <Button type='link' onClick={() => { }}>View Question</Button>
+            </Space>
+          )}
+        />
+      </Table>
+    )
+
+    return (
+      <Fragment>
+        <Row>
+          <AntBreadcrumb
+            elements={[
+              { text: 'Admin', to: '/admin' },
+              { text: 'Dashboard' },
+            ]}
+          />
+        </Row>
+        <Row>
+          <Col span={18}>
+            <Carousel autoplay dotPosition={'right'} style={{ margin: '1.5rem', padding: '1.5rem' }}>
+              <Bar {...labChartConfig} />
+              <Bar {...theoryChartConfig} />
+            </Carousel>
+          </Col>
+          <Col span={6}>
+            <Card style={{ height: 'auto', margin: '1.5rem', textAlign: 'center' }} title='Upcoming Class'>
+              <h4>A1.606</h4>
+              <h5>Software Project Management</h5>
+              <h5>Laboratory</h5>
+              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'baseline' }}>
+                <WarningTwoTone twoToneColor='#AAAA00' />
+                <p>5 students have been absent for 2 sessions</p>
+              </div>
+              <Button type='primary' onClick={() => { }}>Open Session</Button>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            {table}
+          </Col>
+        </Row>
+      </Fragment>
     )
   }
 }
--- src/pages/Management/Management.js
+++ src/pages/Management/Management.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route } from "react-router-dom";
 
 import 'antd/dist/antd.css';
-import { Layout } from 'antd';
+import { Layout, Avatar, Badge } from 'antd';
 
 import AntMenu from '../../components/Management/Menu/AntMenu'
 import Classes from './Classes/Classes';
@@ -18,16 +18,25 @@ class Management extends React.Component {
       <Layout style={{ minHeight: '100vh' }}>
         <AntMenu />
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Header className="site-layout-background" style={{ padding: 0, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+
+            <h5 style={{ margin: '0 1.5rem', color: '#FFFFFF' }}>QuizDance</h5>
+            <Badge count={5} style={{ margin: '0 1.5rem' }}>
+              <Avatar
+                size={{ xs: 10, sm: 14, md: 18, lg: 30, xl: 34, xxl: 36 }}
+                style={{ color: '#f56a00', backgroundColor: '#fde3cf', margin: '0 1.5rem' }}
+              >C</Avatar>
+            </Badge>
+          </Header>
           <Content style={{ margin: '0 16px' }}>
             <Switch>
               <Route path={`${path}`} exact component={Dashboard} />
               <Route path={`${path}/classes`} exact component={Classes} />
               <Route path={`${path}/sessions/:classId`} exact component={Sessions} />
-              <Route path={`${path}/reports`} exact component={()=><h2>Currently under development D:</h2>} />
+              <Route path={`${path}/reports`} exact component={() => <h2>Currently under development D:</h2>} />
             </Switch>
           </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          <Footer style={{ textAlign: 'center', padding: '12px 50px' }}>QuizDance ©2020 Created by SPM8</Footer>
         </Layout>
       </Layout>
     );
