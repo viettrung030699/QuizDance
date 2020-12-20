4b2d4dc8849ea2b424445a132cee8f50d051aeb5
viettrung030699 <viettrungho030699@gmail.com>
2020-12-20 11:23:17 +0700

    Fix deploy

--- src/components/Lecturer/LecturerSite.js
+++ src/components/Lecturer/LecturerSite.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./LecturerSite.scss";
 import io from "socket.io-client";
 const socket = io("http://quizdance.herokuapp.com");
